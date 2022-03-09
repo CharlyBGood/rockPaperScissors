d66de749b32de165226fa0ed13a1fa2eddd3b167
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-03-09 00:06:55 -0300

    build functions

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+let playButton = document.querySelector(".play");
+playButton.addEventListener("click", playRound);
+
 function computerPlay() {
   let computerResult = "";
   let computerSelect = ["Rock", "Scissor", "Paper"];
   computerResult = Math.floor(Math.random() * computerSelect.length);
-  console.log(computerSelect[computerResult]);
+    console.log(computerSelect[computerResult]);
   return computerResult;
 }
 
-computerPlay();
+function playRound(computerSel, playerSel) {
+//     const newLocal = 'Paper';
+//    if (playerSel == 'Scissor' & computerSel == 'Rock' || newLocal) {
+       
+//        return 'You lose';
+//    } else if(playerSel == 'Rock' & computerSel == 'Paper')
+//         return 'You lose Again!'
+    
+    if (playerSel == 'Scissor' & computerSel == 'Rock' || 'Paper') {
+        return 'You lose'
+    }
+    
+}
 
-
+const playerSel = window.prompt("Rock, Scissor or Paper?");
+const computerSel = computerPlay();
+// console.log(playRound(playerSel, computerSel));
