cb81973aa5ab3ee0b0676e41fddef087b17919e4
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-03-10 15:40:18 -0300

    make function work

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-let playButton = document.querySelector(".play");
-playButton.addEventListener("click", playRound);
-
 let computerResult = "";
 let computerSelect = ["Rock", "Scissor", "Paper"];
 
@@ -10,14 +7,36 @@ function computerPlay() {
   return computerSelect[computerResult];
 }
 
-function playRound(computerSel, playerSel) {
-  computerSel = computerSelect;
-  if (computerSel == 'Rock' || 'Scissor' || 'Paper') {
-    console.log('yes')
-  } 
+let playButton = document.querySelector(".play");
+playButton.addEventListener("click", playRound);
+
+function playRound(computerSele, playerSele) {
+  if (computerSel == 'Rock' && playerSel == 'Scissor') {
+    return ('you lose');
+  } else if (computerSel == 'Scissor' && playerSel == 'Paper') {
+    return ('you lose too')
+  } else if (computerSel == 'Paper' && playerSel == 'Rock') {
+    return ('you fockin lose again')
+  } else if (computerSel == 'Scissor' && playerSel == 'Scissor') {
+    return ('It is a draw!');
+  } else if (computerSel == 'Scissor' && playerSel == 'Rock') {
+    return ('You won!!');
+  } else if (computerSel == 'Paper' && playerSel == 'Scissor') {
+    return ('you won again!');
+  } else if (computerSel == 'Rock' && playerSel == 'Paper') {
+    return ('you won yeah motherfucker!')
+  }  else if (computerSel == 'Rock' && playerSel == 'Rock') {
+    return ('DRAW AGAIN')
+  } else if (computerSel == 'Paper' && playerSel == 'Paper') {
+    return ('DRAW')
+ } else {
+    return ('you wrote cualquiera wacho')
+  }
+  
 }
 
-const playerSel = prompt('Rock, Scissor or Paper?');
-const computerSel = computerPlay();
-console.log(playerSel)
-console.log(computerSel);
+  const playerSel = prompt('Rock, Scissor or Paper?');
+  const computerSel = computerPlay();
+  console.log(playerSel, computerSel);
+const resultGame = playRound();
+console.log(resultGame);
