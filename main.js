8177de6f1a482949f98c818892092e63cc6437fc
CHARLY_BGOOD <bonavita.carlos@gmail.com>
2022-03-10 18:47:03 -0300

    change function

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
 let computerResult = "";
-let computerSelect = ["Rock", "Scissor", "Paper"];
+let rockScissorPaper = ["Rock", "Scissor", "Paper"];
 
 function computerPlay() {
-  computerResult = Math.floor(Math.random() * computerSelect.length);
-  // console.log(computerSelect[computerResult]);
-  return computerSelect[computerResult];
+  computerResult = Math.floor(Math.random() * rockScissorPaper.length);
+  // console.log(rockScissorPaper[computerResult]);
+  return rockScissorPaper[computerResult];
 }
 
 let playButton = document.querySelector(".play");
 playButton.addEventListener("click", playRound);
 
-function playRound(computerSele, playerSele) {
-  if (computerSel == 'Rock' && playerSel == 'Scissor') {
+function playRound(computer, player) {
+  player = playerSel;
+  computer = computerSel;
+  if (computer == rockScissorPaper[0] && player == rockScissorPaper[1]) {
     return ('you lose');
-  } else if (computerSel == 'Scissor' && playerSel == 'Paper') {
+  } else if (computer == rockScissorPaper[1] && player == rockScissorPaper[2]) {
     return ('you lose too')
-  } else if (computerSel == 'Paper' && playerSel == 'Rock') {
+  } else if (computer == rockScissorPaper[2] && player == rockScissorPaper[0]) {
     return ('you fockin lose again')
-  } else if (computerSel == 'Scissor' && playerSel == 'Scissor') {
-    return ('It is a draw!');
-  } else if (computerSel == 'Scissor' && playerSel == 'Rock') {
-    return ('You won!!');
-  } else if (computerSel == 'Paper' && playerSel == 'Scissor') {
-    return ('you won again!');
-  } else if (computerSel == 'Rock' && playerSel == 'Paper') {
-    return ('you won yeah motherfucker!')
-  }  else if (computerSel == 'Rock' && playerSel == 'Rock') {
-    return ('DRAW AGAIN')
-  } else if (computerSel == 'Paper' && playerSel == 'Paper') {
+  } else if (computer ==  player) {
     return ('DRAW')
- } else {
+ } else if (computer == rockScissorPaper[1] && player == rockScissorPaper[0]) {
+    return ('you won!')
+ } else if (computer == rockScissorPaper[2] && player == rockScissorPaper[1]) {
+  return ('you won again!')
+} else if (computer == rockScissorPaper[0] && player == rockScissorPaper[2]) {
+  return ('you won!')
+} else {
     return ('you wrote cualquiera wacho')
   }
-  
 }
 
   const playerSel = prompt('Rock, Scissor or Paper?');
   const computerSel = computerPlay();
-  console.log(playerSel, computerSel);
-const resultGame = playRound();
-console.log(resultGame);
+  // console.log(playerSel, computerSel);
+
